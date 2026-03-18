b7ef4dd7da88d9163926e8592f7e0aebe13e7611
Marcos Frenkel <Marcosf2@illinois.edu>
2026-03-18 01:13:53 -0500

    power rabi

--- src/cqedtoolbox/measurement_lib/qick/single_transmon_v2.py
+++ src/cqedtoolbox/measurement_lib/qick/single_transmon_v2.py
@@ -177,43 +177,48 @@ class AmplitudeRabiProgram(AveragerProgramV2):
     Note that the current version of the QICK firmware supports 16-bit signed gain.
     '''
     def _initialize(self, cfg):
-        ro_ch = cfg['ro_ch']
-        ro_gen_ch = cfg['ro_gen_ch']
-        q_gen_ch = cfg['q_gen_ch']
+        ro_adc_ch = cfg['ro_adc_ch']
+        ro_dac_ch = cfg['ro_dac_ch']
+        q_dac_ch = cfg['q_dac_ch']
 
-        self.declare_gen(ch=q_gen_ch, nqz=cfg['q_nqz'])
-        self.declare_gen(ch=ro_gen_ch, nqz=cfg['ro_nqz'])
-        self.declare_readout(ch=ro_ch, length=cfg['ro_len'])
+        self.declare_gen(ch=ro_dac_ch, nqz=cfg['ro_nqz'])
+        self.declare_readout(ch=ro_adc_ch, length=cfg['ro_len'])
+
+        self.declare_gen(ch=q_dac_ch, nqz=cfg['q_nqz'])
+
         
         # Pump pulse definition
-        self.add_loop("q_ge_gain_loop", self.cfg["steps"])
-        self.add_gauss(ch=q_gen_ch, name="gauss", sigma=cfg['q_ge_sig'], length=4*cfg['q_ge_sig'], even_length=True)        
-        self.add_pulse(ch=q_gen_ch, name="pi_pulse",  
-                       style="arb", 
-                       envelope="gauss", 
-                       freq=cfg['q_ge'], 
-                       phase=cfg['q_ge_phase'],
-                       gain=cfg['q_ge_gains'],  # Sweep variable 
-                      )
+        self.add_loop("q_gains_loop", self.cfg["q_gain_steps"])
+        self.add_gauss(ch=q_dac_ch, name="gauss", sigma=cfg['q_pi_sigma'], length=cfg['q_pi_n_sigma'] * cfg['q_pi_sigma'], even_length=True)
+        self.add_pulse(ch=q_dac_ch, name="pi_pulse",
+                       style="arb",
+                       envelope="gauss",
+                       freq=cfg['q_freq'],
+                       phase=cfg['q_pi_phase'],
+                       gain=cfg['q_gain_loop_var'],  # Sweep variable
+                       )
         
         # Probe pulse definition
-        self.add_readoutconfig(ch=ro_ch, name="myro", freq=cfg['ro_freq'], gen_ch=ro_gen_ch)
-        self.add_pulse(ch=ro_gen_ch, name="read_pulse", ro_ch=ro_ch, 
-                       style="const", 
-                       freq=cfg['ro_freq'], 
+        self.add_readoutconfig(ch=ro_adc_ch, name="readout", freq=cfg['ro_freq'], gen_ch=ro_dac_ch)
+        self.add_pulse(ch=ro_dac_ch, name="read_pulse", ro_ch=ro_adc_ch,
+                       style="const",
+                       freq=cfg['ro_freq'],
                        length=cfg['ro_len'],
                        phase=cfg['ro_phase'],
-                       gain=cfg['ro_gain'], 
-                      )
+                       gain=cfg['ro_gain'],
+                       )
         
     def _body(self, cfg):
-        self.send_readoutconfig(ch=cfg['ro_ch'], name="myro", t=0)
+        ro_adc_ch = cfg['ro_adc_ch']
+        ro_dac_ch = cfg['ro_dac_ch']
+        q_dac_ch = cfg['q_dac_ch']
 
-        self.pulse(ch=cfg['q_gen_ch'], name='pi_pulse', t=0)
+        self.send_readoutconfig(ch=ro_adc_ch, name="readout", t=0)
+
+        self.pulse(ch=q_dac_ch, name='pi_pulse', t=0)
         self.delay_auto(t=0.0, gens=True, ros=False, tag='wait_time')
-        self.pulse(ch=cfg['ro_gen_ch'], name="read_pulse", t=0)
-        self.trigger(ros=[cfg['ro_ch']], pins=[0], t=cfg['trig_time'])
-
+        self.pulse(ch=ro_dac_ch, name="read_pulse", t=0)
+        self.trigger(ros=[ro_adc_ch], pins=[0], t=cfg['trig_time'])
 
 
 @QickBoardSweep(
